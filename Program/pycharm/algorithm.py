41757190bf2e34af8fdbfa88c3d54795db8fd309
YE H <canary4651@gmail.com>
2019-11-21 20:11:08 +0900

    algorithm

---
+++ Program/pycharm/algorithm.py
@@ -0,0 +1,18 @@
+import math
+#절대값 알고리즘
+# 입력 : 실수 a
+# 출력 : a의 절댓값
+
+def abs_sign(a):
+    if a >= 0
+        return a
+    else:
+        return -a
+
+def abs_square(a):
+    b = a * a
+    return math.sqrt(b)
+
+
+def abs_square_sum(a):
+    for i in abs_square_sum(i):
