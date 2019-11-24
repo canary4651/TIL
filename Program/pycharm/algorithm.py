338f1dfcf768826b2ba1a2b56d57e21b2ff2bdcb
YE H <canary4651@gmail.com>
2019-11-24 23:20:24 +0900

    algorithm

--- Program/pycharm/algorithm.py
+++ Program/pycharm/algorithm.py
@@ -3,16 +3,29 @@ import math
 # 입력 : 실수 a
 # 출력 : a의 절댓값
 
-def abs_sign(a):
-    if a >= 0
-        return a
-    else:
-        return -a
+# def abs_sign(a):
+#     if a >= 0
+#         return a
+#     else:
+#         return -a
+#
+# def abs_square(a):
+#     b = a * a
+#     return math.sqrt(b)
+#
+#
+# def abs_square_sum(a):
+#     for i in abs_square_sum(i):
 
-def abs_square(a):
-    b = a * a
-    return math.sqrt(b)
+#
+def find_same_name(a):
+    n = len(a)
+    result = set()
+    for i in range(0,n-1):
+        for j in range(i+1,n):
+            if a[i]==a[j]:
+                result.add(a[i])
+    return result
 
-
-def abs_square_sum(a):
-    for i in abs_square_sum(i):
+name = ['tom','jerry','mike','tom']
+print(find_same_name(name))
